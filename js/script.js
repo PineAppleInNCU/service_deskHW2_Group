f5f173c6ea3358ff3fad50367b1f8e9434756e07
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-07-27 02:34:55 +0800

    finish the ghost touch player and then i must figour out the safe area

--- js/script.js
+++ js/script.js
@@ -33,6 +33,12 @@ div_2.style.top='300px';
 div_3.style.left = '300px';
 div_3.style.top='500px';
 
+
+var div=[];
+div[0]=div_1;
+div[1]=div_2;
+div[2]=div_3;
+
 document.addEventListener("keydown", keyDownHandler, false); //add keylistener
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -288,60 +294,203 @@ function checkBoarder_3(){//第一個角色的邊界判斷
 
 
 
+var ghost_change_bool=false;
+var ghost_number=0;
+var first_time=true;//判斷是否第一次random
+var last_ghost_number;
+var other_player=[];//除了鬼以外，其他玩家的編號
+
+
+
+div[0].style.backgroundColor="black";
+div[1].style.backgroundColor="black";
+div[2].style.backgroundColor="black";
+
+
+
+var div_Xs=[];
+var div_Ys=[];
+
+
+
+
 
 function move(){
 	////////////////////every movement
+	//除了動作以外，變成鬼的程式碼也可以放在這裡
+
+
+/////////////抽鬼是誰
+	if(!ghost_change_bool){
+		if(first_time){
+			ghost_number= Math.floor((Math.random() * 3) + 1)-1;//從0~2中選一個數字
+		}
+		else{//從第二次開始，就要紀錄上一個ghost_number，以避免ghost_number重複
+
+
+			last_ghost_number=ghost_number;
+
+			while(last_ghost_number===ghost_number){
+				ghost_number= Math.floor((Math.random() * 3) + 1)-1;
+			}
+		}
+		//div[ghost_number].style.backgroundColor="red";
+		console.log(ghost_number);
+		if(ghost_number===0){
+			
+
+			document.getElementById("character_2").style.backgroundColor="black";
+			document.getElementById("character_3").style.backgroundColor="black";
+
+			document.getElementById("character_1").style.backgroundColor="red";
+
+			other_player[0]=1;
+			other_player[1]=2;
+		}
+		else if(ghost_number===1){
+			
+
+			document.getElementById("character_1").style.backgroundColor="black";
+			document.getElementById("character_3").style.backgroundColor="black";
+			document.getElementById("character_2").style.backgroundColor="red";
+
+			other_player[0]=0;
+			other_player[1]=2;
+		}
+		else if(ghost_number===2){
+			
+
+			document.getElementById("character_1").style.backgroundColor="black";
+			document.getElementById("character_2").style.backgroundColor="black";
+			document.getElementById("character_3").style.backgroundColor="red";
+
+			other_player[0]=0;
+			other_player[1]=1;
+		}
+		ghost_change_bool=true;
+	}
+////////////抽鬼是誰
+
+
+
+
+
+
+/////////////每個角色的操控
 	if(checkBoarder_1()){
 		if(rightPressed_1){
-			div_1.style.left = parseInt(document.getElementById("character_1").style.left)+x_speed+'px';
+			div[0].style.left = parseInt(document.getElementById("character_1").style.left)+x_speed+'px';
 		}
 		else if(leftPressed_1){
-			div_1.style.left = parseInt(document.getElementById("character_1").style.left)-x_speed+'px';
+			div[0].style.left = parseInt(document.getElementById("character_1").style.left)-x_speed+'px';
 		}
 		else if(upPressed_1){
-			div_1.style.top = parseInt(document.getElementById("character_1").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
+			div[0].style.top = parseInt(document.getElementById("character_1").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
 		}
 		else if(downPressed_1){
-			div_1.style.top = parseInt(document.getElementById("character_1").style.top)+y_speed+'px';
+			div[0].style.top = parseInt(document.getElementById("character_1").style.top)+y_speed+'px';
 		}
 	}
 
 	if(checkBoarder_2()){
 		if(rightPressed_2){
-			div_2.style.left = parseInt(document.getElementById("character_2").style.left)+x_speed+'px';
+			div[1].style.left = parseInt(document.getElementById("character_2").style.left)+x_speed+'px';
 		}
 		else if(leftPressed_2){
-			div_2.style.left = parseInt(document.getElementById("character_2").style.left)-x_speed+'px';
+			div[1].style.left = parseInt(document.getElementById("character_2").style.left)-x_speed+'px';
 		}
 		else if(upPressed_2){
-			div_2.style.top = parseInt(document.getElementById("character_2").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
+			div[1].style.top = parseInt(document.getElementById("character_2").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
 		}
 		else if(downPressed_2){
-			div_2.style.top = parseInt(document.getElementById("character_2").style.top)+y_speed+'px';
+			div[1].style.top = parseInt(document.getElementById("character_2").style.top)+y_speed+'px';
 		}
 	}
 	if(checkBoarder_3()){
 		if(rightPressed_3){
-			div_3.style.left = parseInt(document.getElementById("character_3").style.left)+x_speed+'px';
+			div[2].style.left = parseInt(document.getElementById("character_3").style.left)+x_speed+'px';
 		}
 		else if(leftPressed_3){
-			div_3.style.left = parseInt(document.getElementById("character_3").style.left)-x_speed+'px';
+			div[2].style.left = parseInt(document.getElementById("character_3").style.left)-x_speed+'px';
 		}
 		else if(upPressed_3){
-			div_3.style.top = parseInt(document.getElementById("character_3").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
+			div[2].style.top = parseInt(document.getElementById("character_3").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
 		}
 		else if(downPressed_3){
-			div_3.style.top = parseInt(document.getElementById("character_3").style.top)+y_speed+'px';
+			div[2].style.top = parseInt(document.getElementById("character_3").style.top)+y_speed+'px';
 		}
 	}
+/////////////每個角色的操控  END
+
+
+///////
+	div_Xs[0]=parseInt(document.getElementById("character_1").style.left);
+	div_Xs[1]=parseInt(document.getElementById("character_2").style.left);
+	div_Xs[2]=parseInt(document.getElementById("character_3").style.left);
+	div_Ys[0]=parseInt(document.getElementById("character_1").style.top);
+	div_Ys[1]=parseInt(document.getElementById("character_2").style.top);
+	div_Ys[2]=parseInt(document.getElementById("character_3").style.top);
+
+
+
+	/*
+	if(第一個other_player撞到鬼){
+		遊戲結束。進行分數結算
+	}
+	else if(第二個other_player撞到鬼){
+		遊戲結束，進行分數結算
+	}
+*/
+   
+	if((div_Xs[other_player[0]]<=(div_Xs[ghost_number]+55) && div_Xs[other_player[0]]>=div_Xs[ghost_number] &&
+	   div_Ys[other_player[0]]<=(div_Ys[ghost_number]+55) && div_Ys[other_player[0]]>=(div_Ys[ghost_number]) )/*完成左上角的偵測*/ ||
+	  ((div_Xs[other_player[0]]+55)<=(div_Xs[ghost_number]+55) && (div_Xs[other_player[0]]+55)>=div_Xs[ghost_number] &&
+	   div_Ys[other_player[0]]<=(div_Ys[ghost_number]+55) && div_Ys[other_player[0]]>=(div_Ys[ghost_number]))/*完成右上角的偵測*/||
+	  ((div_Xs[other_player[0]])<=(div_Xs[ghost_number]+55) && (div_Xs[other_player[0]])>=div_Xs[ghost_number] &&
+	   (div_Ys[other_player[0]]+55)<=(div_Ys[ghost_number]+55) && (div_Ys[other_player[0]]+55)>=(div_Ys[ghost_number]))/*完成左下角的偵測*/||
+	  ((div_Xs[other_player[0]]+55)<=(div_Xs[ghost_number]+55) && (div_Xs[other_player[0]]+55)>=div_Xs[ghost_number] &&
+	   (div_Ys[other_player[0]]+55)<=(div_Ys[ghost_number]+55) && (div_Ys[other_player[0]]+55)>=(div_Ys[ghost_number]))
+
+		){
+		test.innerHTML='ghost_'+(ghost_number+1)+":touch player_"+(other_player[0]+1);
+	}
+	else if((div_Xs[other_player[1]]<=(div_Xs[ghost_number]+55) && div_Xs[other_player[1]]>=div_Xs[ghost_number] &&
+	   div_Ys[other_player[1]]<=(div_Ys[ghost_number]+55) && div_Ys[other_player[1]]>=(div_Ys[ghost_number]) )/*完成左上角的偵測*/ ||
+	  ((div_Xs[other_player[1]]+55)<=(div_Xs[ghost_number]+55) && (div_Xs[other_player[1]]+55)>=div_Xs[ghost_number] &&
+	   div_Ys[other_player[1]]<=(div_Ys[ghost_number]+55) && div_Ys[other_player[1]]>=(div_Ys[ghost_number]))/*完成右上角的偵測*/||
+	  ((div_Xs[other_player[1]])<=(div_Xs[ghost_number]+55) && (div_Xs[other_player[1]])>=div_Xs[ghost_number] &&
+	   (div_Ys[other_player[1]]+55)<=(div_Ys[ghost_number]+55) && (div_Ys[other_player[1]]+55)>=(div_Ys[ghost_number]))/*完成左下角的偵測*/||
+	  ((div_Xs[other_player[1]]+55)<=(div_Xs[ghost_number]+55) && (div_Xs[other_player[1]]+55)>=div_Xs[ghost_number] &&
+	   (div_Ys[other_player[1]]+55)<=(div_Ys[ghost_number]+55) && (div_Ys[other_player[1]]+55)>=(div_Ys[ghost_number]))
+){
+		test.innerHTML='ghost_'+(ghost_number+1)+":touch player_"+(other_player[1]+1);
+	}
+///////撞到鬼
+
+	//if(div[ghost_number]碰觸到其他方塊，則結束遊戲。並依靠結束遊戲的方法，判斷誰輸誰贏)
+
 	requestAnimationFrame(move);
 
-
-
-
 }
+
+
+
+
+function setGhost_change_bool(){
+	if(ghost_change_bool){
+		ghost_change_bool=false;
+		//div[ghost_number].style.backgroundColor="black";
+		//ghost_number=Math.random()*(2-0)+0;
+	}
+}
+setInterval(setGhost_change_bool,10000);//將ghost_change_bool改回false
+
 move();
 
-
-
 alert("a");
+
+/*
+	現在要做做看鬼
+	碰到鬼後會發生alert (因為只是要測試)
+
+*/
