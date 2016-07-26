e83d439d3fc37d45fe6518d03d78e908dec66cc6
PineAppleInNCU <fdgkhdkgh@gmail.com>
2016-07-26 15:23:57 +0800

    add the boarder checking for character 1

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ var downPressed=false;
 
 var x=1000;
 var div =document.getElementById("character_1"); //要使用這條程式，.html裡的js引入要擺在最下方，因為若js引入式擺在最上方的話，dom還未建立，則會出現null錯誤
+
+
+var test=document.getElementById("test");
+
+test.innerHTML='test';
+
+
 div.style.left = '0px';
 div.style.top='0px';
 
@@ -14,10 +21,11 @@ document.addEventListener("keyup", keyUpHandler, false);
 
 
 
-var x=20;
-var y=20;
+var x_speed=10;//這個值變了的話，checkBoarder的數值也要變
+var y_speed=10;
 
 function keyDownHandler(e) {//the action of the key listener
+	/////////////////////////////
         if(e.keyCode == 39) {
             rightPressed = true;
            
@@ -38,7 +46,7 @@ function keyDownHandler(e) {//the action of the key listener
 
         	//div.style.top = parseInt(document.getElementById("character_1").style.top)+y+'px';
         }
-
+    //////////////////////////////character_1
 
 }
 function keyUpHandler(e) {
@@ -57,32 +65,76 @@ function keyUpHandler(e) {
         else if(e.keyCode == 40){
         	downPressed = false ;
         }
+    //////////////////////////////character_1
 
 }
 
-if(rightPressed){
-	div.style.left = '100px';
+
+
+function checkBoarder(){//邊界判斷
+	var boarder_left=0;
+	var boarder_top=0;
+	var boarder_right=1000;
+	var boarder_bottom=550;
+
+	var character_1_x=parseInt(document.getElementById("character_1").style.left);
+	var character_1_y=parseInt(document.getElementById("character_1").style.top);
+
+	test.innerHTML=character_1_x+"  "+character_1_y;
+
+
+		if(rightPressed){
+			if((character_1_x)>(boarder_right-55)){
+				return false;
+			}
+		}
+		else if(leftPressed){
+			if(character_1_x<(boarder_left+10)){
+				return false;
+			}
+		}
+		else if(upPressed){
+			if(character_1_y<boarder_top+10){
+				return false;
+			}
+		}
+		else if(downPressed){
+			if(character_1_y>boarder_bottom-55){
+				return false;
+			}
+		}
+
+
+
+	return true;
 }
-else{
-	div.style.left='0px';
-}
+
 
 
 function move(){
-	if(rightPressed){
-		div.style.left = parseInt(document.getElementById("character_1").style.left)+x+'px';
-	}
-	else if(leftPressed){
-		div.style.left = parseInt(document.getElementById("character_1").style.left)-x+'px';
-	}
-	else if(upPressed){
-		div.style.top = parseInt(document.getElementById("character_1").style.top)-y+'px';//若要使用此列程式碼，要先初始化
-	}
-	else if(downPressed){
-		div.style.top = parseInt(document.getElementById("character_1").style.top)+y+'px';
+	////////////////////every movement
+	if(checkBoarder()){
+		if(rightPressed){
+			div.style.left = parseInt(document.getElementById("character_1").style.left)+x_speed+'px';
+		}
+		else if(leftPressed){
+			div.style.left = parseInt(document.getElementById("character_1").style.left)-x_speed+'px';
+		}
+		else if(upPressed){
+			div.style.top = parseInt(document.getElementById("character_1").style.top)-y_speed+'px';//若要使用此列程式碼，要先初始化
+		}
+		else if(downPressed){
+			div.style.top = parseInt(document.getElementById("character_1").style.top)+y_speed+'px';
+		}
 	}
 	requestAnimationFrame(move);
+
+
+
+
 }
 move();
+
+
 
 alert("a");
